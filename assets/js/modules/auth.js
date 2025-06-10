4eda2f414edf08f105913a6621b57493c7c19d49
magnatavr <benoyevskiy@mail.ru>
2025-06-10 17:46:08 +0300

    done with some tasks review

--- assets/js/modules/auth.js
+++ assets/js/modules/auth.js
@@ -3,6 +3,17 @@ export function toggleAuth() {
   const loginForm = document.querySelector(".auth__form--login");
   const registerForm = document.querySelector(".auth__form--register");
 
+  const url = new URL(window.location.href);
+  const paramValue = url.searchParams.get("name");
+
+  if (paramValue === "login") {
+    loginForm.classList.remove("hidden");
+    registerForm.classList.add("hidden");
+  } else if (paramValue === "register") {
+    loginForm.classList.add("hidden");
+    registerForm.classList.remove("hidden");
+  }
+
   authLink.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
